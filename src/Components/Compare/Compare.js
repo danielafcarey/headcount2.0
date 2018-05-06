d01d238b025fdef6474d0b93dc9297989fd9f84c
Valentino Valentin <valentinovalentine95@gmail.com>
2018-05-06 17:17:29 -0600

    refactor --COMPARE

--- src/Components/Compare/Compare.js
+++ src/Components/Compare/Compare.js
@@ -5,49 +5,49 @@ import PropTypes from 'prop-types';
 const Compare = ({ compareCards, compareData, removeCompareCard }) => {
   const selectedCards = compareCards.map((district, index) => {
     const title = Object.keys(district)[0];
-    const listOfData = Object.values(district)[0] 
+    const listOfData = Object.values(district)[0]; 
 
-    return <Card title={title}
-                listOfData={listOfData}
+    return <Card title={ title }
+                listOfData={ listOfData }
                 section='compare'
-                removeCompareCard={removeCompareCard}
+                removeCompareCard={ removeCompareCard }
                 selected={ district.selected }
-                key={`compareCard${index}`}
-    />
+                key={ `compareCard${index}` }
+    />;
 
-  })
+  });
 
   function createCompareCard() {
     if (compareCards.length === 2 && compareData) {
-      const compareDataEntries = Object.entries(compareData)
-      const district1Title = compareDataEntries[0][0]
-      const district1Average = compareDataEntries[0][1]
-      const district2Title = compareDataEntries[1][0]
-      const district2Average = compareDataEntries[1][1]
-      const compared = compareDataEntries[2][1]
+      const compareDataEntries = Object.entries(compareData);
+      const district1Title = compareDataEntries[0][0];
+      const district1Average = compareDataEntries[0][1];
+      const district2Title = compareDataEntries[1][0];
+      const district2Average = compareDataEntries[1][1];
+      const compared = compareDataEntries[2][1];
 
       return (
           <div className="card compare-data">
-            <h3>{district1Title}: {district1Average}</h3>
-            <h3>{compared}</h3>
-            <h3>{district2Title}: {district2Average}</h3>
+            <h3>{ district1Title }: { district1Average }</h3>
+            <h3>{ compared }</h3>
+            <h3>{ district2Title }: { district2Average }</h3>
           </div>
-      )
+      );
     }
   }
 
   return (
     <div className="card-container compare-cards">
-     {selectedCards}
+     { selectedCards }
      { createCompareCard() } 
     </div>
-  )
+  );
 }
 
 Compare.propTypes = {
   compareCards: PropTypes.arrayOf(PropTypes.object).isRequired,
   compareData: PropTypes.object,
   removeCompareCard: PropTypes.func.isRequired
-}
+};
 
 export default Compare;
